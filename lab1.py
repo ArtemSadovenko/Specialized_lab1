4324a2d50acf74d242ede00321ae0c40f43bb6f8
Artem Sadovenko <asadovenko7@gmail.com>
2023-10-04 03:36:33 +0300

    commit

--- lab1.py
+++ lab1.py
@@ -1,50 +1,5 @@
 import math
 
-# def calculator():
-#     while True:
-#         try:
-#             num1 = float(input("Введіть перше число: "))
-#             operator = input("Введіть оператор (+, -, *, /, ^, √, %): ")
-#             if operator not in ('+', '-', '*', '/', '^', '√', '%'):
-#                 print("Недопустимий оператор. Спробуйте ще раз.")
-#                 continue
-#             if operator == '^':
-#                 num2 = float(input("Введіть ступінь: "))
-#             elif operator == '√':
-#                 num2 = None
-#             else:
-#                 num2 = float(input("Введіть друге число: "))
-
-#             if operator == '+':
-#                 result = num1 + num2
-#             elif operator == '-':
-#                 result = num1 - num2
-#             elif operator == '*':
-#                 result = num1 * num2
-#             elif operator == '/':
-#                 result = num1 / num2
-#             elif operator == '^':
-#                 result = num1 ** num2
-#             elif operator == '√':
-#                 result = math.sqrt(num1)
-#             elif operator == '%':
-#                 result = num1 % num2
-
-#             print("Результат:", result)
-
-#             if input("Бажаєте продовжити (Так/Ні)? ").lower() != 'так':
-#                 break
-#         except ValueError:
-#             print("Будь ласка, введіть дійсне число.")
-#         except ZeroDivisionError:
-#             print("Ділення на нуль неможливе.")
-#         except Exception as e:
-#             print("Сталася помилка:", e)
-
-# if __name__ == "__main__":
-#     calculator()
-
-
 while True:
         try:
             num1 = float(input("Enter num1: "))
